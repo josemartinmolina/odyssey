68f43dfd8e8c11cd608018069eb8bca258c8cdcd
josemartinmolina <jose.molina@tec.mx>
2023-02-15 08:56:27 -0600

    actualizacion pagina

--- game/views.py
+++ game/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1> Hola Mundo!</h1>')
+    return HttpResponse('<h1> Bienvenido a la página inicial!</h1>')
