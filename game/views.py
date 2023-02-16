490b90b301e62da95cac24abf8579e32826dd26e
josemartinmolina <jose.molina@tec.mx>
2023-02-16 10:04:35 -0600

    modificacion de la vista

--- game/views.py
+++ game/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1> Bienvenidos a la sesión del jueves!</h1>')
+    #return HttpResponse('<h1> Bienvenidos a la sesión del jueves!</h1>')
+    return render(request, 'index.html')
