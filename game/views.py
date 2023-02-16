6604311c8b2e6067e8f6f73d7e9d86b33135be43
josemartinmolina <jose.molina@tec.mx>
2023-02-16 09:21:31 -0600

    Nuevo letrero

--- game/views.py
+++ game/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1> Bienvenido a la página inicial!</h1>')
+    return HttpResponse('<h1> Bienvenidos a la sesión del jueves!</h1>')
