93e92339cb27dc9a023088d32d3f96361cb37c79
José Martín Molina <75451788+josemartinmolina@users.noreply.github.com>
2023-02-27 14:59:34 -0600

    Apertura a todos las direcciones IP

--- odyssey/settings.py
+++ odyssey/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7^=63p_-wkiq!!!c1=*nubsoe-m0tavfpqpa*43b)hs)ns+6oa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
